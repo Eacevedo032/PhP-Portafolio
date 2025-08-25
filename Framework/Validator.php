dd20370a026bc56181785f076384a3f331ee535f
EAcevedo <131281511+Eacevedo032@users.noreply.github.com>
2025-08-25 16:03:33 -0600

    refactorizacion en el codigo de validator.php

--- Framework/Validator.php
+++ Framework/Validator.php
@@ -20,15 +20,7 @@ class Validator
             foreach ($rules as $rule) {
                 [$name, $parameter] = array_pad(explode(':', $rule, 2), 2, null);
 
-                $error = match ($name) {
-                    'required' => empty($value)                                             ? "$field is required"                              : null,
-                    'min'      => strlen($value) < (int)$parameter                  ? "$field must be at least $parameter characters"   : null,
-                    'max'      => strlen($value) > (int)$parameter                  ? "$field must be at most $parameter characters"    : null,
-                    'url'      => !filter_var($value, FILTER_VALIDATE_URL)   ? "$field must be a valid URL"                      : null,
-                    default    => null,
-                };
-
-                if ($error) {
+                if ($error = $this->hasError($name, $parameter, $field, $value)) {
                     $this->errors[] = $error;
 
                     break;
@@ -37,6 +29,21 @@ class Validator
         }
     }
 
+    protected function hasError(string $name, string $parameter, string $field, $value): ?string
+    {
+        return match ($name) {
+            'required' => $this->validateRequired($field, $value),
+            'min'      => strlen($value) < (int)$parameter                  ? "$field must be at least $parameter characters"   : null,
+            'max'      => strlen($value) > (int)$parameter                  ? "$field must be at most $parameter characters"    : null,
+            'url'      => !filter_var($value, FILTER_VALIDATE_URL)   ? "$field must be a valid URL"                      : null,
+            default    => null,
+        };
+    }
+
+    protected function validateRequired(string $field, mixed $value): ?string
+    {
+        return ($value === null || $value === '') ? "$field is required" : null;
+    }
     public function passes(): bool
     {
         return empty($this->errors);
